f3c85e46fe0ac7d98499ab1dc6e18c7ebc0779db
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-28 22:59:09 -0600

    agregar cache

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { FormsModule } from '@angular/forms';
-import { Login_Port } from 'src/app/config/Interface/Login/Login.port';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { PerfilComponent } from './views';
+import { HistorialventaComponent } from './views';
+import { VentaComponent } from './views';
+import { MenuNavegacionComponent } from './views';
+
 import { RegistroComponent } from './views/registro/registro.component';
+
+import { IniciarSesion_Adapter } from './config/Connection/IniciarSesion/IniciarSesion.adapter';
+import { IniciarSesion_Port } from 'src/app/config/Interface/IniciarSesion/IniciarSesion.port';
 import { IniciarSesionComponent } from './views/iniciar-sesion/iniciar-sesion.component';
-import { CacheComponent } from './views/cache/cache.component';
-import { Login_Adapter } from './config/Connection/Login/Login.adapter';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
     IniciarSesionComponent,
-    CacheComponent
+    PerfilComponent,
+    HistorialventaComponent,
+    VentaComponent,
+    MenuNavegacionComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-    {provide: Login_Port, useClass: Login_Adapter},
+    {provide: IniciarSesion_Port, useClass: IniciarSesion_Adapter},
+
   ],
   bootstrap: [AppComponent]
 })
