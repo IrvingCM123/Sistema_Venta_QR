7c14b2e1a78ed2d649ef6de8661f20f1e3ef89e1
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-30 19:11:39 -0600

    cambios en diseño

--- src/app/views/personal/personal.component.ts
+++ src/app/views/personal/personal.component.ts
@@ -1,19 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-personal',
-  templateUrl: './personal.component.html',
-  styleUrls: ['./personal.component.scss']
+    selector: "app-personal",
+    templateUrl: "./personal.component.html",
+    styleUrls: ["./personal.component.scss"],
 })
 export class PersonalComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    empleados = [
+        {
+            Nombre: "Irving Rafael",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            Sucursal: 12331,
+        },
+        {
+            Nombre: "Diana Laura",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar8.png",
+            Sucursal: 12331
+        },
+        {
+            Nombre: "Elizabeth",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar3.png",
+            Sucursal: 12331
+        },
+        {
+            Nombre: "Ruben",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar4.png",
+            Sucursal: 12331
+        },
+        {
+            Nombre: "Irving Rafael",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar7.png",
+            Sucursal: 12331,
+        },
+        {
+            Nombre: "Diana Laura",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar8.png",
+            Sucursal: 12331
+        },
+        {
+            Nombre: "Elizabeth",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar3.png",
+            Sucursal: 12331
+        },
+        {
+            Nombre: "Ruben",
+            Rango: "Empleado",
+            Imagen: "https://bootdey.com/img/Content/avatar/avatar4.png",
+            Sucursal: 12331
+        }
+    ];
 
-  empleado = [
-    {Nombre: "Irving Rafael", Rango: "Empleado", Imagen: "URL", "Sucursal": 12331 }
-  ]
 
+    MostrarEmpleado() {
+        console.log("Hola Mundo")
+    }
 }
