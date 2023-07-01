8b09439ef50575c4a552642e73a5adfd726986f2
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-07-01 01:06:08 -0600

    Diseño NavBar

--- src/app/views/menu-navegacion/menu-navegacion.component.ts
+++ src/app/views/menu-navegacion/menu-navegacion.component.ts
@@ -1,76 +1,76 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 declare var $: any;
-import * as jQuery from 'jquery';
-
+import * as jQuery from "jquery";
 
 @Component({
-  selector: "app-menu-navegacion",
-  templateUrl: "./menu-navegacion.component.html",
-  styleUrls: ["./menu-navegacion.component.scss"],
+    selector: "app-menu-navegacion",
+    templateUrl: "./menu-navegacion.component.html",
+    styleUrls: ["./menu-navegacion.component.scss"],
 })
 export class MenuNavegacionComponent implements OnInit {
-  constructor() {}
+    constructor() {}
 
-  ngOnInit(): void {
-    const test = () => {
-        console.log("Hoa")
-      const tabsNewAnim = $("#navbarSupportedContent");
-      const selectorNewAnim = $("#navbarSupportedContent").find("li").length;
-      const activeItemNewAnim = tabsNewAnim.find(".active");
-      const activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-      const activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-      const itemPosNewAnimTop = activeItemNewAnim.position();
-      const itemPosNewAnimLeft = activeItemNewAnim.position();
-      $(".hori-selector").css({
-        top: itemPosNewAnimTop.top + "px",
-        left: itemPosNewAnimLeft.left + "px",
-        height: activeWidthNewAnimHeight + "px",
-        width: activeWidthNewAnimWidth + "px",
-      });
-      $("#navbarSupportedContent").on("click", "li", (e: Event) => {
-        $("#navbarSupportedContent ul li").removeClass("active");
-        $(e.target).addClass("active");
-        const activeWidthNewAnimHeight = $(e.target).innerHeight();
-        const activeWidthNewAnimWidth = $(e.target).innerWidth();
-        const itemPosNewAnimTop = $(e.target).position();
-        const itemPosNewAnimLeft = $(e.target).position();
-        $(".hori-selector").css({
-          top: itemPosNewAnimTop.top + "px",
-          left: itemPosNewAnimLeft.left + "px",
-          height: activeWidthNewAnimHeight + "px",
-          width: activeWidthNewAnimWidth + "px",
+    ngOnInit(): void {
+        const test = () => {
+            const tabsNewAnim = $("#navbarSupportedContent");
+            const selectorNewAnim = $("#navbarSupportedContent").find(
+                "li"
+            ).length;
+            const activeItemNewAnim = tabsNewAnim.find(".active");
+            const activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+            const activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+            const itemPosNewAnimTop = activeItemNewAnim.position();
+            const itemPosNewAnimLeft = activeItemNewAnim.position();
+            $(".hori-selector").css({
+                top: itemPosNewAnimTop.top + "px",
+                left: itemPosNewAnimLeft.left + "px",
+                height: activeWidthNewAnimHeight + "px",
+                width: activeWidthNewAnimWidth + "px",
+            });
+            $("#navbarSupportedContent").on("click", "li", (e: Event) => {
+                $("#navbarSupportedContent ul li").removeClass("active");
+                $(e.target).addClass("active");
+                const activeWidthNewAnimHeight = $(e.target).innerHeight();
+                const activeWidthNewAnimWidth = $(e.target).innerWidth();
+                const itemPosNewAnimTop = $(e.target).position();
+                const itemPosNewAnimLeft = $(e.target).position();
+                $(".hori-selector").css({
+                    top: itemPosNewAnimTop.top + "px",
+                    left: itemPosNewAnimLeft.left + "px",
+                    height: activeWidthNewAnimHeight + "px",
+                    width: activeWidthNewAnimWidth + "px",
+                });
+            });
+        };
+
+        $(document).ready(() => {
+            setTimeout(() => {
+                test();
+            });
         });
-      });
-    };
 
-    $(document).ready(() => {
-      setTimeout(() => {
-        test();
-      });
-    });
+        $(window).on("resize", () => {
+            setTimeout(() => {
+                test();
+            }, 500);
+        });
 
-    $(window).on("resize", () => {
-      setTimeout(() => {
-        test();
-      }, 500);
-    });
+        $(".navbar-toggler").click(() => {
+            $(".navbar-collapse").slideToggle(300);
+            setTimeout(() => {
+                test();
+            });
+        });
 
-    $(".navbar-toggler").click(() => {
-      $(".navbar-collapse").slideToggle(300);
-      setTimeout(() => {
-        test();
-      });
-    });
-
-    jQuery(document).ready(($: any) => {
-      let path = window.location.pathname.split("/").pop();
-      if (path == "") {
-        path = "index.html";
-      }
-      const target = $(
-        '#navbarSupportedContent ul li a[href="' + path + '"]'
-      );
-      target.parent().addClass("active");
-    });
-  }
+        jQuery(document).ready(($: any) => {
+            let path = window.location.pathname.split("/").pop();
+            if (path == "") {
+                path = "index.html";
+            }
+            const target = $(
+                '#navbarSupportedContent ul li a[href="' + path + '"]'
+            );
+            target.parent().addClass("active");
+        });
+    }
 }
