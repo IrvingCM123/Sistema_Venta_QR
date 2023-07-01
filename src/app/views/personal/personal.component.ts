7f208ad971570e7a5d2d48bb7431d37782cc521c
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-30 20:14:31 -0600

    agregar scroll de personal

--- src/app/views/personal/personal.component.ts
+++ src/app/views/personal/personal.component.ts
@@ -16,53 +16,61 @@ export class PersonalComponent implements OnInit {
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar7.png",
             Sucursal: 12331,
+            Numero_Personal: 13512,
         },
         {
             Nombre: "Diana Laura",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar8.png",
-            Sucursal: 12331
+            Sucursal: 12331,
+            Numero_Personal: 12512,
         },
         {
             Nombre: "Elizabeth",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar3.png",
-            Sucursal: 12331
+            Sucursal: 12331,
+            Numero_Personal: 14512,
         },
         {
             Nombre: "Ruben",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar4.png",
-            Sucursal: 12331
+            Sucursal: 12331,
+            Numero_Personal: 15512,
         },
         {
             Nombre: "Irving Rafael",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar7.png",
             Sucursal: 12331,
+            Numero_Personal: 16512,
         },
         {
             Nombre: "Diana Laura",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar8.png",
-            Sucursal: 12331
+            Sucursal: 12331,
+            Numero_Personal: 17512,
         },
         {
             Nombre: "Elizabeth",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar3.png",
-            Sucursal: 12331
+            Sucursal: 12331,
+            Numero_Personal: 18512,
         },
         {
             Nombre: "Ruben",
             Rango: "Empleado",
             Imagen: "https://bootdey.com/img/Content/avatar/avatar4.png",
-            Sucursal: 12331
-        }
+            Sucursal: 12331,
+            Numero_Personal: 19512,
+        },
     ];
 
-
-    MostrarEmpleado() {
-        console.log("Hola Mundo")
+    MostrarEmpleado(Numero_Personal: string | number) {
+        console.log(Numero_Personal);
+        window.location.href = 'Sistema/Empleados'
     }
 }
